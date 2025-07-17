2b856fb71b682da2a7af209c63b24094dbcf9659
jamie <JCLacivita@gmail.com>
2025-07-17 12:59:33 -0600

    added logging

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,7 +1,17 @@
+import logging
+
+formatstring = "%(asctime)s %(message)s"
+fo = logging.Formatter(formatstring)
+sh = logging.StreamHandler()
+sh.setFormatter(fo)
+l = logging.getLogger(__name__)
+l.setLevel(logging.DEBUG)
+l.addHandler(sh)
 
 def helloworld():
     """Stub function to set up environment"""
     print(f"Hello World.")
+    l.debug("JWTO")
     return 5
 
 
